95fa05ce2714e3da16ed7405a00f17b80e7fb431
confidentlygeeky <32813764+confidentlygeeky@users.noreply.github.com>
2018-06-23 15:03:13 +0530

    Spring Rest

---
+++ SpringHibCRUD/src/main/java/com/niit/UserService/UserService.java
@@ -0,0 +1,16 @@
+package com.niit.UserService;
+
+import java.util.List;
+
+import com.niit.CRUDhibernatemodel.User;
+
+public interface UserService {
+
+	
+	public boolean addUser(User user);
+	public User getUserById(int userId);
+	public List<User> getuserList();
+	public boolean updateUser(User user);
+	public boolean deleteUser(User user);
+	public User getByUsername(String username);
+}
---
+++ SpringRestMVC/src/main/java/com/niit/controller/UserRestController.java
@@ -0,0 +1,66 @@
+package com.niit.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.CRUDhibernatemodel.User;
+import com.niit.UserService.UserService;
+
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
+public class UserRestController {
+
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("/")
+	public ResponseEntity<List<User>> getUserList() {
+		userService.getuserList();
+		return new ResponseEntity<List<User>>(userService.getuserList(),HttpStatus.OK);		
+	}
+	
+	@PostMapping()
+	public ResponseEntity <Void> addUser(@RequestBody User user){
+		
+		userService.addUser(user);
+		return new ResponseEntity<Void>(HttpStatus.CREATED);
+		
+	}
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity <User> getUserById(@PathVariable("userId") int userId){
+		return new ResponseEntity<User>(userService.getUserById(userId),HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity <Void> deleteUser(@PathVariable("userId") int userId){
+		User user=userService.getUserById(userId);
+		userService.deleteUser(user);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+		
+		@PutMapping("/")
+		public ResponseEntity<Void> updateUser(@RequestBody User user) {
+		
+		userService.updateUser(user);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+		}
+		
+		@PostMapping("/user")
+		public ResponseEntity <User> getUserByUsername(@RequestBody User user){
+			return new ResponseEntity<User>(userService.getByUsername(user.getUsername()),HttpStatus.OK);
+		}
+	}
+	
+
